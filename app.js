d6c11e25fbbef1f2e03b81d6a55b9650856e1d72
yalcindo <yalcindo1@gmail.com>
2013-11-01 14:20:07 -0600

    Adding new quizes

--- app.js
+++ app.js
@@ -21,47 +21,81 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
+var GlobalVar={
+  language:"spa"
+};
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
+//beglobal api 
 var beglobal = new BeGlobal.BeglobalAPI({
   api_token: 'OwS3JrQzSHtHfckSfvEZUA%3D%3D'
 });
-
+// dummyData for Random Words
+ var dummyData=[["apple","pear","melon","orange","banana","cherry","fig","avocado"],
+ ["red","blue","green","yellow","grey","black","white","dark","light"],["street","road","alley","avenue","court","intersection","house","home"]];
 
 app.get('/', routes.index);
-app.get('/translate',function(req,res){
-	
+//renders application's page
+app.get('/translate',function(req,res){	
   res.render("translate");
 });
-app.get('/quiz',function(req,res){
-	
+//renders quiz page
+app.get('/quiz',function(req,res){	
   res.render("quiz");
 });
-
-
+//use beglobal api,translates word sends back to client
 app.get("/translatework",function(req,res){
-	var info=req.query;
-
+	  var info=req.query;
     beglobal.translations.translate(info,
-	  function(err, results) {
-	          console.log("results",results)
+	  function(err, results) {       
 	    res.send(results);
     });
 });
+//Dummy data goes to client
 app.get("/quizwork",function(req,res){
-	var langTrans=req.query.lang;
-    var dummyData={word1:"apple",word2:"pear",word3:"orange",word4:"cherry",
-    word5:"melon",word6:"banana",word7:"carrot",word8:"fig"};
+  //the practiced language
+	GlobalVar.language=req.query.lang;
+  console.log("lagtranslate:",GlobalVar.language)
 	res.send(dummyData);
 });
+// Dummy data gets translated
 
+app.get("/quizanswer",function(req,res) {
+  var info=req.query;
+  console.log("info:",info)
+  var bgData={
+    text:info.mytext,
+    from:GlobalVar.language,
+    to:info.to
+  };
 
+  beglobal.translations.translate(bgData,
+  function(err, results) {
+    if (err) {
+      return console.log(err);
+    }
 
+    console.log("results in bg data",results);
+    res.send(results);
+  });
+ 
+});
+app.get("/quizcorrectanswer",function(req,res){
+  var info=req.query;
+  info.to=GlobalVar.language;
+  console.log("quiz correct answer:",info);
+  beglobal.translations.translate(info,
+  function(err, results) {
+    if (err) {
+      return console.log(err);
+    }
 
-
+    console.log("resultsin correct answer",results);
+    res.send(results);
+  });
+})
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,43 +1,148 @@
 $(function(){
+	//encapsulating data related values
+	  var counters={
+		quizCounter:1,
+		correctCounter:0,
+		wrongCounter:0,
+		nextQuiz:[],
+		quizIndex:0,
+		quizNum:1,
+		quizLang:"spa"
+      };
 
-     //the translation form is processed
+    var createQuiz=function(word){
+        var sourceQuiz = $("#quiz-template").html();
+		var quizTemplate = Handlebars.compile(sourceQuiz);
+		return $(".quiz-div").html(quizTemplate({words:word}));
+    };
+    var restartQuiz=function(word){
+        var sourceQuiz = $("#restart-template").html();
+		var quizTemplate = Handlebars.compile(sourceQuiz);
+		return $(".quiz-div").html(quizTemplate({words:word}));
+    };
+	var createNextQuiz=function(word){
+		var sourceQuiz = $("#next-template").html();
+		var quizTemplate = Handlebars.compile(sourceQuiz);
+		return $(".quiz-div").html(quizTemplate({words:word}));
+    };
+    var createCorrectRespond = function(divInput,message){
+		return divInput.html("<div>"+message+"<br>"+
+			"<button type='button' class='btn btn-primary btn-next'>Next</button>"+
+						  	"</div>");
+    };
+    var createWrongRespond=function(divInput,message){
+        return  divInput.html("<div> WRONG!!!"+"<br>"+"Correct Answer: "+message+"<br>"+
+            "<button type='button' class='btn btn-primary btn-next'>Next</button>"+
+						  "</div>");
+    };
+    var checkQuizAnswer=function(){
+    	  if(counters.wrongCounter===3){
+        	restartQuiz("you made three mistakes start over");
+        	counters.quizCounter=0;
+        	counters.wrongCounter=0;
+        }else if(counters.quizCounter<8){
+	    	createQuiz(counters.nextQuiz[counters.quizIndex][counters.quizCounter]);
+	    	counters.quizCounter++;
+	    }
+       else {
+        	createNextQuiz("Go to the next Quiz");
+        	counters.quizIndex++;
+        	counters.quizCounter=0;
+        	counters.wrongCounter=0;
+        }
+    };
+    var createQuizSuccess =function(word){
+    	var sourceQuiz = $("#quizSuccess-template").html();
+		var quizTemplate = Handlebars.compile(sourceQuiz);
+		return $(".quizsuccess-div").append(quizTemplate({words:word}));
+
+    };
+    var createQuizEnd = function(word){
+    	var sourceQuiz = $("#quizEnd-template").html();
+		var quizTemplate = Handlebars.compile(sourceQuiz);
+		return $(".quiz-div").html(quizTemplate({words:word}));
+
+    };
+// --------------------Translation Section
+
 	$("#translate-form").submit(function(e){
 		e.preventDefault();
 		var translateFrom = $("#lang-from").val();
-		var translateTo =  $("#lang-to").val();
+		var translateTo = $("#lang-to").val();
 		var theWord = $("#word").val();
-		//creating data object to send to server
-		var data={text:theWord,
-		       from:translateFrom,
-		       to:translateTo
-		      };
 		//the translatework route sends data 
 		//to server and get backs processed data
-        $.get("/translatework",data,function(data)
+        $.get("/translatework",{text:theWord,from:translateFrom,to:translateTo},function(data1)
         {            
 	        var source = $("#translation-template").html();
 			var translationTemplate = Handlebars.compile(source);
-		    $(".translation-div").html(translationTemplate(data));
+		    $(".translation-div").html(translationTemplate(data1));
 	    });      
 	});
-    
+	// -----------------------QUIZ Section------------------------
     //quiz event handler
+    // creates the initial quiz question
     $("#quiz-form").submit(function(e){
     	e.preventDefault();
-    	var quizLang=$("#quiz-dropdown").val();
-    	var data={lang:quizLang};
-        $.get("/quizwork",data,function(data){
-           console.log("data:",data)
-           var sourceQuiz = $("#quiz-template").html();
-			var quizTemplate = Handlebars.compile(sourceQuiz);
-
-		    $(".quiz-div").append(quizTemplate({words:data}));
-
+    	counters.quizLang=$("#quiz-dropdown").val();
+    	var data2={lang:counters.quizLang};
+        $.get("/quizwork",data2,function(data2){
+        	//dummydata is @nextQuiz
+            counters.nextQuiz=data2;
+			createQuiz(data2[0][0]);		
         });
     });
 
- 
+   //?????quiz  input on keyup is not working ask the question
+   // dynamicall created content
+    $(".quiz-div").on("keyup",".quiz-input",function(e){
 
+		if(e.keyCode===13){
+			var val = $(this).val();
+			var wordCompare=$(this).parent().data("id");
+			var $inputLoc=$(this).parent();
+		
+            var getCorrectAnswer=function(jqueryObject,word){	
+                return $.get("/quizcorrectanswer",{text:word,from: "eng",to: "spa"},function(data){
+                  createWrongRespond(jqueryObject,data.translation);
+            	});
+            	
+            };
 
+			$.get("/quizanswer",{mytext: val,from: "spa",to: "eng"},function(data){
+				
+				if(data.translation.toUpperCase()===wordCompare.toUpperCase())
+				{
+					counters.correctCounter++;
+					createCorrectRespond($inputLoc,"Good job");
+				}
+				else
+				{
+					counters.wrongCounter++;
+					getCorrectAnswer($inputLoc,wordCompare);  
+				}
+			});
+		}
+    });
+   // clik next button
+    $(".quiz-div").on("click",".btn-next",function(e){
+    	checkQuizAnswer();
+    });
+    // if the user enters three wrong answers,it starts index 0
+    $(".quiz-div").on("click",".btn-restart",function(e){
+   		checkQuizAnswer();
+    });
 
+    $(".quiz-div").on("click",".btn-nextQuiz",function(e){
+        if(counters.quizNum === 3){
+        	console.log("counters.quizNum: ",counters.quizNum);
+	     	createQuizEnd("You completed three succesful Quizes");
+    	}else{
+	    	checkQuizAnswer();
+	    	console.log("counters.quizNum: ",counters.quizNum);
+	    	createQuizSuccess("quiz " + counters.quizNum + " successful");
+	    	
+	    	counters.quizNum++;
+     }
+    });
 });
